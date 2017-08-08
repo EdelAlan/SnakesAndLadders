25e84980136f18258604339df1ebe8292bd8556a
Alan Edel <my.magic@mail.ru>
2017-08-08 15:49:26 +0800

    added snakes and ladders. making move mechanism is separated from startGame() method to new makeMove() method.

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Random;
 
 public class Game {
@@ -5,7 +6,7 @@ public class Game {
     private Player player2;
     private int p1roll;
     private int p2roll;
-    private int dice;
+    private HashMap<Integer, Integer> sal = new HashMap<>();
 
     public Game (Player player1, Player player2) {
         this.player1 = player1;
@@ -17,34 +18,47 @@ public class Game {
     public void startGame() throws InterruptedException {
         System.out.println();
 
+        sal.put(25, 35);
+        sal.put(50, 60);
+        sal.put(75, 85);
+        sal.put(30, 20);
+        sal.put(45, 35);
+        sal.put(99, 0);
 
         while (true) {
-            int currentPos1 = player1.getPosition();
-            int currentPos2 = player2.getPosition();
 
-            p1roll = throwDice();
-            player1.setPosition(player1.getPosition() + p1roll);
-            System.out.println(player1.getName() + " rolled " + p1roll + ", position from " +currentPos1 + " to " + player1.getPosition());
-            Thread.sleep(250);
-
+        //–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– Player 1
+            makeMove(player1);
             if (player1.getPosition() >= 100 ) {
                 System.out.println(player1.getName() + " is winner!");
                 break;
             }
 
-            p2roll = throwDice();
-            player2.setPosition(player2.getPosition() + p2roll);
-            System.out.println(player2.getName() + " rolled " + p2roll + ", position from " +currentPos2 + " to " + player2.getPosition());
-            Thread.sleep(250);
-
+        //–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– Player 2
+            makeMove(player2);
             if (player2.getPosition() >= 100 ) {
                 System.out.println(player2.getName() + " is winner!");
                 break;
             }
+
         }
+    }
 
+    private void makeMove(Player player) throws InterruptedException {
+        int diceRoll = throwDice();
+        int currentPos = player.getPosition();
 
+        int p = currentPos + diceRoll;
+        Integer value = sal.get(p);
 
+        if (value != null) {
+            player.setPosition(value);
+            System.out.println(player.getName() + " rolled " + diceRoll + ", and found something, position from " + p + " to " + player.getPosition());
+        } else {
+            player.setPosition(p);
+            System.out.println(player.getName() + " rolled " + diceRoll + ", position from " +currentPos + " to " + player.getPosition());
+        }
+        Thread.sleep(250);
     }
 
     private int throwDice() {
